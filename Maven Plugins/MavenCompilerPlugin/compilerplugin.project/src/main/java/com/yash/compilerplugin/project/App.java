dfc01e549a9f9425bb7abd5c00f0eeaa003b3bff
Yashwanth Parameswaran <Parameswaran.Yashwanth@in.bosch.com>
2023-01-29 16:47:12 +0530

    Maven Learnings

---
+++ Maven Plugins/MavenCompilerPlugin/compilerplugin.project/src/main/java/com/yash/compilerplugin/project/App.java
@@ -0,0 +1,13 @@
+package com.yash.compilerplugin.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
